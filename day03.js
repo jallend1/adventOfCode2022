7cb78d3e3db3a8e51e09e9eacd65b33860b09399
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-06 14:00:51 -0800

    Completes part one of Day 03

--- day03.js
+++ day03.js
@@ -2,14 +2,36 @@
 const fs = require('fs');
 const rawRucksackData = fs.readFileSync('day03-input.txt', 'utf8').split('\n');
 
-rawRucksackData.forEach((rucksackData) => {
-  const compartmentOne = rucksackData.slice(
-    0,
-    Math.floor(rucksackData.length / 2)
-  );
-  const compartmentTwo = rucksackData.slice(
-    Math.floor(rucksackData.length / 2)
-  );
-  console.log(compartmentOne);
-  console.log(compartmentTwo);
-});
+let totalPriority = 0;
+
+const findCommonCharacter = (strOne, strTwo) => {
+  const arrOne = strOne.split('');
+  const arrTwo = strTwo.split('');
+  return arrOne.filter((element) => arrTwo.includes(element)).join('');
+};
+
+const calculatePriority = (commonLetter) => {
+  if (commonLetter.charCodeAt(0) < 91) {
+    return commonLetter.charCodeAt(0) - 38;
+  } else {
+    return commonLetter.charCodeAt(0) - 96;
+  }
+};
+
+const sortRucksacks = (rawRucksackData) => {
+  rawRucksackData.forEach((rucksackData) => {
+    const compartmentOne = rucksackData.slice(
+      0,
+      Math.floor(rucksackData.length / 2)
+    );
+    const compartmentTwo = rucksackData.slice(
+      Math.floor(rucksackData.length / 2)
+    );
+    const commonLetter = findCommonCharacter(compartmentOne, compartmentTwo);
+    totalPriority += calculatePriority(commonLetter);
+  });
+};
+
+sortRucksacks(rawRucksackData);
+
+console.log(totalPriority);
