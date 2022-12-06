e5cceb645e49a2d55af58664628252c339bd5a84
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-05 20:22:43 -0800

    Adds input file from day three's problem

---
+++ day03.js
@@ -0,0 +1,16 @@
+// Day 03 of Advent of Code 2022
+const fs = require('fs');
+const rawRucksackData = fs.readFileSync('day03-input.txt', 'utf8').split('\n');
+
+rawRucksackData.forEach((rucksackData) => {
+  //   const rucksack = rucksackData.split('');
+  const compartmentOne = rucksackData.slice(
+    0,
+    Math.floor(rucksackData.length / 2)
+  );
+  const compartmentTwo = rucksackData.slice(
+    Math.floor(rucksackData.length / 2)
+  );
+  console.log(compartmentOne);
+  console.log(compartmentTwo);
+});
