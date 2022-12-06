bade3ad9e99aff545595e68c3911306c35c79894
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-05 20:09:04 -0800

    Completes Part 2 of Day 2

--- day02.js
+++ day02.js
@@ -2,18 +2,21 @@
 const fs = require('fs');
 const rawElfData = fs.readFileSync('day02-input.txt', 'utf8').split('\n');
 
-const rockPaperScissorsRules = {
+const rules = {
   A: {
+    // Rock
     X: 3,
     Y: 6,
     Z: 0
   },
   B: {
+    // Paper
     X: 0,
     Y: 3,
     Z: 6
   },
   C: {
+    // Scissors
     X: 6,
     Y: 0,
     Z: 3
@@ -21,21 +24,47 @@ const rockPaperScissorsRules = {
 };
 
 const shapePoints = {
-  X: 1,
-  Y: 2,
-  Z: 3
+  X: 1, // One point for rock
+  Y: 2, // Two points for paper
+  Z: 3 // Three points for scissors
 };
 
-let score = 0;
-
-const determineWinner = (elfOne, elfTwo) => {
-  return rockPaperScissorsRules[elfOne][elfTwo] + shapePoints[elfTwo];
+const strategyGuide = {
+  X: 0, // X means lose
+  Y: 3, // Y means tie
+  Z: 6 // Z means win
 };
 
-rawElfData.forEach((elfData) => {
-  const [elfOne, elfTwo] = elfData.split(' ');
-  const winner = determineWinner(elfOne, elfTwo);
-  score += winner;
-});
+const partOneSolution = () => {
+  let score = 0;
 
-console.log(score);
+  const determineWinner = (elfOne, elfTwo) => {
+    // Combine the points for the game outcome with the points associated with the particular shape
+    return rules[elfOne][elfTwo] + shapePoints[elfTwo];
+  };
+
+  rawElfData.forEach((elfData) => {
+    const [elfOne, elfTwo] = elfData.split(' ');
+    const winner = determineWinner(elfOne, elfTwo);
+    score += winner;
+  });
+  console.log('Misinterpreted Strategy Guide Solution (Problem #1): ' + score);
+};
+
+const partTwoSolution = () => {
+  let score = 0;
+  rawElfData.forEach((elfData) => {
+    const [elfOne, elfTwo] = elfData.split(' ');
+    Object.keys(rules[elfOne]).find((shape) => {
+      // Finds the shape that will result in the prescribed outcome from the strategy guide
+      if (rules[elfOne][shape] === strategyGuide[elfTwo]) {
+        // Adds the points for the game outcome with the points associated with the particular shape
+        score += rules[elfOne][shape] + shapePoints[shape];
+      }
+    });
+  });
+  console.log('Strategy Guide Solution (Problem #2): ' + score);
+};
+
+partOneSolution();
+partTwoSolution();
