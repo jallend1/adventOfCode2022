cca27af265d2e4fb360ebfe016e497843f4ff2de
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-05 19:22:37 -0800

    Cleans up my solution to part one of Day 02

--- day02.js
+++ day02.js
@@ -2,33 +2,40 @@
 const fs = require('fs');
 const rawElfData = fs.readFileSync('day02-input.txt', 'utf8').split('\n');
 
-const determineWinner = (elfOne, elfTwo) => {
-  if (elfOne === 'A') {
-    if (elfTwo === 'X') return 3 + 1;
-    if (elfTwo === 'Y') return 6 + 2;
-    if (elfTwo === 'Z') return 0 + 3;
-  } else if (elfOne === 'B') {
-    if (elfTwo === 'X') return 0 + 1;
-    if (elfTwo === 'Y') return 3 + 2;
-    if (elfTwo === 'Z') return 6 + 3;
-  } else if (elfOne === 'C') {
-    if (elfTwo === 'X') return 6 + 1;
-    if (elfTwo === 'Y') return 0 + 2;
-    if (elfTwo === 'Z') return 3 + 3;
+const rockPaperScissorsRules = {
+  A: {
+    X: 3,
+    Y: 6,
+    Z: 0
+  },
+  B: {
+    X: 0,
+    Y: 3,
+    Z: 6
+  },
+  C: {
+    X: 6,
+    Y: 0,
+    Z: 3
   }
 };
 
-// const calculateScore = (elf) => {
-//   if (elf === 'A' || elf === 'X') return 1;
-//   if (elf === 'B' || elf === 'Y') return 2;
-//   if (elf === 'C' || elf === 'Z') return 3;
-// };
-let sum = 0;
+const shapePoints = {
+  X: 1,
+  Y: 2,
+  Z: 3
+};
+
+let score = 0;
+
+const determineWinner = (elfOne, elfTwo) => {
+  return rockPaperScissorsRules[elfOne][elfTwo] + shapePoints[elfTwo];
+};
+
 rawElfData.forEach((elfData) => {
   const [elfOne, elfTwo] = elfData.split(' ');
   const winner = determineWinner(elfOne, elfTwo);
-  sum += winner;
-  console.log(winner);
+  score += winner;
 });
 
-console.log(sum);
+console.log(score);
