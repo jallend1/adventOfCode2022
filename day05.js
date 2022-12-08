64f2f8d370ef5dc47a7018847baaf0afae02d749
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-07 18:25:50 -0800

    Completes Day 05 of Advent of Code

--- day05.js
+++ day05.js
@@ -3,17 +3,6 @@
 const fs = require('fs');
 const inputFile = fs.readFileSync('day05-input.txt', 'utf8').split('\n');
 
-// The crate layout to help me visualize:
-// '    [H]         [D]     [P]        ',
-// '[W] [B]         [C] [Z] [D]        ',
-// '[T] [J]     [T] [J] [D] [J]        ',
-// '[H] [Z]     [H] [H] [W] [S]     [M]',
-// '[P] [F] [R] [P] [Z] [F] [W]     [F]',
-// '[J] [V] [T] [N] [F] [G] [Z] [S] [S]',
-// '[C] [R] [P] [S] [V] [M] [V] [D] [Z]',
-// '[F] [G] [H] [Z] [N] [P] [M] [N] [D]',
-// ' 1   2   3   4   5   6   7   8   9 ',
-
 const extractCrates = (line) => {
   const crates = [];
   for (let i = 1; i <= 33; i += 4) {
@@ -35,29 +24,31 @@ const extractMovementData = (rawData) => {
   return movementData;
 };
 
-const rawMovementData = inputFile.slice(10);
-const movementData = extractMovementData(rawMovementData);
-
-const rawStackData = inputFile.slice(0, 8);
-const stackData = [...Array(9)].map(() => []);
-
-rawStackData.forEach((line) => {
-  const crateContents = extractCrates(line);
-  crateContents.map((crate, index) => {
-    // If blank space, we got no crates on that stack
-    if (crate !== ' ') {
-      stackData[index].push(crate);
-    }
+const compileStackData = (rawStackData) => {
+  rawStackData.forEach((line) => {
+    const crateContents = extractCrates(line);
+    crateContents.map((crate, index) => {
+      // If blank space, we got no crates on that stack
+      if (crate !== ' ') {
+        stackData[index].push(crate);
+      }
+    });
   });
-});
+};
 
-const processMoveOrder = (numOfCrates, sourceStack, destinationStack) => {
+const crateMover9000 = (numOfCrates, sourceStack, destinationStack) => {
   // Takes the top crate (First in the array) and moves it to the destination stack
   // Repeats the process for the number of crates specified
   for (let i = 0; i < numOfCrates; i++) {
     const movedCrate = stackData[sourceStack].shift();
     stackData[destinationStack].unshift(movedCrate);
   }
+};
+
+const crateMover9001 = (numOfCrates, sourceStack, destinationStack) => {
+  // CrateMover9001 is capable of moving multiple crates at once
+  const movedCrates = stackData[sourceStack].splice(0, numOfCrates);
+  stackData[destinationStack].unshift(...movedCrates);
 };
 
 const moveCrates = (movementData) => {
@@ -67,7 +58,8 @@ const moveCrates = (movementData) => {
       sourceStack: source,
       destinationStack: destination
     } = moveOrder;
-    processMoveOrder(crates, source - 1, destination - 1);
+    crateMover9000(crates, source - 1, destination - 1);
+    crateMover9001(crates, source - 1, destination - 1);
   });
 };
 
@@ -80,5 +72,13 @@ const findTopCrates = (stackData) => {
   return topCrates;
 };
 
+const rawMovementData = inputFile.slice(10);
+const movementData = extractMovementData(rawMovementData);
+
+const rawStackData = inputFile.slice(0, 8);
+const stackData = [...Array(9)].map(() => []);
+
+compileStackData(rawStackData);
 moveCrates(movementData);
+
 console.log(findTopCrates(stackData));
