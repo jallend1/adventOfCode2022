0abeef85c7f4a42ece13d3c02f889c99600a7dcd
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-07 18:58:40 -0800

    Completes Day 06 of Advent of Code 2022

--- day06.js
+++ day06.js
@@ -3,20 +3,18 @@
 const fs = require('fs');
 const code = fs.readFileSync('day06-input.txt', 'utf8');
 
-let index;
-
-const findMarker = (code) => {
-  for (let markerStart = 0; markerStart < code.length - 4; markerStart++) {
+const findDistinctCharacters = (num, string) => {
+  for (let startIndex = 0; startIndex < string.length - num; startIndex++) {
     const uniqueCharacters = new Set();
-    for (let char = markerStart; char < markerStart + 4; char++) {
-      uniqueCharacters.add(code[char]);
+    for (let char = startIndex; char < startIndex + num; char++) {
+      uniqueCharacters.add(string[char]);
     }
-    if (uniqueCharacters.size === 4) {
-      index = markerStart + 4;
-      return uniqueCharacters;
+    if (uniqueCharacters.size === num) {
+      console.log(startIndex + num);
+      return;
     }
   }
 };
 
-console.log(findMarker(code));
-console.log(index);
+findDistinctCharacters(4, code);
+findDistinctCharacters(14, code);
