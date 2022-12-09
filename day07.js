3396fb49b3b8952d7979abb6bbe5de5d8f2acb66
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-12-08 18:46:19 -0800

    Scaffolds out basic structure for solution to Day 07 Part 01

--- day07.js
+++ day07.js
@@ -1,4 +1,18 @@
 // Day 07 of Advent of Code 2022
 
 const fs = require('fs');
-const commands = fs.readFileSync('day07-input.txt', 'utf8').split('\n');
+const commandLines = fs.readFileSync('day07-input.txt', 'utf8').split('\n');
+
+console.log(commandLines);
+
+let directories = [];
+let totalSize = 0;
+
+const handleCommands = (command) => {};
+const handleFiles = (command) => {};
+
+commandLines.forEach((commandLine) => {
+  const command = commandLine.split(' ');
+  if (command[0] === '$') handleCommands(command);
+  else if (command[0] !== 'dir') handleFiles(command);
+});
